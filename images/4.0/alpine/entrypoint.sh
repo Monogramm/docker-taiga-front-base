e9c3a398426761d6f385f5852d6b5ea6a7f29e7b
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-03-20 20:53:53 +0100

    :construction: Fix nginx conf name

--- images/4.0/alpine/entrypoint.sh
+++ images/4.0/alpine/entrypoint.sh
@@ -200,28 +200,29 @@ fi
 # Reinitialize nginx links
 if [ -d /etc/nginx/sites-enabled/ ]; then
   rm /etc/nginx/sites-enabled/*
+else
   mkdir -p /etc/nginx/sites-enabled/
 fi
 
 if [ "$TAIGA_SSL" = "True" ]; then
   if [ -n "$RABBIT_PORT_5672_TCP_ADDR" ]; then
     ln -s \
-      /etc/nginx/sites-available/taiga-ssl \
-      /etc/nginx/sites-enabled/taiga-ssl
+      /etc/nginx/sites-available/taiga-ssl.conf \
+      /etc/nginx/sites-enabled/taiga-ssl.conf
   else
     ln -s \
-      /etc/nginx/sites-available/taiga-ssl \
-      /etc/nginx/sites-enabled/taiga-events-ssl
+      /etc/nginx/sites-available/taiga-ssl.conf \
+      /etc/nginx/sites-enabled/taiga-events-ssl.conf
   fi
 else
   if [ -n "$RABBIT_PORT_5672_TCP_ADDR" ]; then
     ln -s \
-      /etc/nginx/sites-available/taiga \
-      /etc/nginx/sites-enabled/taiga
+      /etc/nginx/sites-available/taiga.conf \
+      /etc/nginx/sites-enabled/taiga.conf
   else
     ln -s \
-      /etc/nginx/sites-available/taiga \
-      /etc/nginx/sites-enabled/taiga-events
+      /etc/nginx/sites-available/taiga.conf \
+      /etc/nginx/sites-enabled/taiga-events.conf
   fi
 fi
 
